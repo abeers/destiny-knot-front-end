6bb91074544e35d4f4fc58b9a9746142fb794a61
abeers <alexbeers92@gmail.com>
2016-11-06 13:47:00 -0500

    Adds an "Add Pokemon" button for unfilled teams
    If a team has less than 6 pokemon, the Add Pokemon button will be
    on the navbar. This currently doesn't do anything, but will allow
    the user to add another Pokemon to the team.

--- app/team/model.js
+++ app/team/model.js
@@ -1,6 +1,10 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  pokemons: DS.hasMany('pokemon')
+  pokemons: DS.hasMany('pokemon'),
+  isNotFull: Ember.computed('pokemons', function(){
+    return this.get('pokemons').get('length') < 6;
+  })
 });
