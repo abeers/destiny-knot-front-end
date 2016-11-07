cd837779f9a8747cd1d5c9cc1a9cbf4b51fa23c1
abeers <alexbeers92@gmail.com>
2016-11-07 13:51:37 -0500

    Adds computed property efficacy
    This computed property makes one efficacy table that combines the
    type_efficacies. Eventually this will account for abilities as well.

--- app/type-efficacy/model.js
+++ app/type-efficacy/model.js
@@ -4,8 +4,7 @@ import Ember from 'ember';
 export default DS.Model.extend({
   damage_factor: DS.attr('number'),
   damage_type_id: DS.attr('number'),
-  // damage_type: DS.belongsTo('type'),
-  damage_type: Ember.computed('target_type', function () {
+  damage_type: Ember.computed(function () {
     return this.get('store').peekRecord('type', this.get('damage_type_id'));
   }),
   target_type: DS.belongsTo('type')
