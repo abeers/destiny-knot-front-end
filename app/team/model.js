2dbf27914783fc2f751c85bcb246970309d6cc04
abeers <alexbeers92@gmail.com>
2016-11-08 09:51:27 -0500

    Fixes addPokemon button bug
    Add Pokemon button was not responding correctly depending on the
    number of pokemon. This was fixed by changing the computed property
    to listen for teamMembers instead of pokemon. Also adds buttons to
    browse and create teams. Create does not do anything right now.

--- app/team/model.js
+++ app/team/model.js
@@ -4,7 +4,7 @@ import Ember from 'ember';
 export default DS.Model.extend({
   name: DS.attr('string'),
   teamMembers: DS.hasMany('team-member'),
-  isNotFull: Ember.computed('pokemons', function(){
+  isNotFull: Ember.computed('teamMembers', function(){
     return this.get('teamMembers').get('length') < 6;
   })
 });
