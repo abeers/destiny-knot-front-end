5b94b7d6059425f589f6d396332983dc04e68de0
abeers <alexbeers92@gmail.com>
2016-11-09 22:07:50 -0500

    Fixes bug with unauthenticated user
    Unauthenticated user can now view all teams. Also adds styling.

--- app/team/model.js
+++ app/team/model.js
@@ -7,8 +7,13 @@ export default DS.Model.extend({
   user: DS.belongsTo('user'),
 
   auth: Ember.inject.service(),
+  isAuthenticated: Ember.computed.alias('auth.isAuthenticated'),
   isEditable: Ember.computed(function() {
-    return this.get('user').get('id').toString() === this.get('auth').get('credentials').get('id').toString();
+    console.log(this.get('isAuthenticated'));
+    if (this.get('isAuthenticated')) {
+      return this.get('user').get('id').toString() === this.get('auth').get('credentials').get('id').toString();
+    }
+    return false;
   }),
 
   isNotFull: Ember.computed('teamMembers', function(){
